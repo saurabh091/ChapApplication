1648b89cb60ca270e1c1d059f4c981f75d83a2ce
Saurabh Rana <rana.saurabh@orangemantra.in>
2019-07-24 11:58:47 +0530

    Initial Commit

---
+++ FirebaseChatApp/FirebaseChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by orangemac05 on 24/07/19.
+//  Copyright © 2019 Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
