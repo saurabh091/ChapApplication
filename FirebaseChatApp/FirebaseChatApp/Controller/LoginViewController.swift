13e50cc4aa5a400f5395abb9e546438a74ddd7ac
Saurabh Rana <saurabhrana091@gmail.com>
2019-07-26 02:30:32 +0530

    LoginviewController textfield added

--- FirebaseChatApp/FirebaseChatApp/Controller/LoginViewController.swift
+++ FirebaseChatApp/FirebaseChatApp/Controller/LoginViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Name"
+        tf.backgroundColor = .white
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email Address"
+        tf.backgroundColor = .white
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    let containerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        
+        view.addSubview(containerStackView)
+        containerStackView.addArrangedSubview(nameTextField)
+        containerStackView.addArrangedSubview(emailTextField)
+        containerStackView.addArrangedSubview(passwordTextField)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -23,6 +61,6 @@ class LoginViewController: UIViewController {
 extension UIColor {
     
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
-        self.init(red: r/255, green: g/255, blue: b/255 alpha:1)
+        self.init(red: r/255, green: g/255, blue: b/255, alpha:1)
     }
 }
