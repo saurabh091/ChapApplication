ab4f2142590773b8c04923524e6683021c026a66
Saurabh Rana <rana.saurabh@orangemantra.in>
2019-08-02 16:05:38 +0530

    User model updated.

---
+++ FirebaseChatApp/FirebaseChatApp/Model/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  FirebaseChatApp
+//
+//  Created by orangemac05 on 02/08/19.
+//  Copyright © 2019 Saurabh. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var name:String
+    var done: Bool
+    var id: String
+    
+    var dictionary: [String: Any] {
+        return [
+            "name": name,
+            "done": done
+        ]
+    }
+}
+
+extension User{
+    init?(dictionary: [String : Any], id: String) {
+        guard   let name = dictionary["name"] as? String,
+            let done = dictionary["done"] as? Bool
+            else { return nil }
+        
+        self.init(name: name, done: done, id: id)
+    }
+}
